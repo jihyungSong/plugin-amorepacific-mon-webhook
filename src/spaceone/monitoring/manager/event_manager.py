01d496e68aa300b8f000d7a316186ebeb4957486
D.Kang <dkang@mz.co.kr>
2021-06-27 03:24:58 +0900

    Update for parsing error for Amore rule

--- src/spaceone/monitoring/manager/event_manager.py
+++ src/spaceone/monitoring/manager/event_manager.py
@@ -5,6 +5,7 @@ from spaceone.core import utils
 from spaceone.core.manager import BaseManager
 from datetime import datetime
 from spaceone.monitoring.model.event_response_model import EventModel
+
 _LOGGER = logging.getLogger(__name__)
 
 TITLE_PARSING_META = [' - ']
@@ -17,18 +18,31 @@ class EventManager(BaseManager):
 
     def parse(self, options, raw_data):
         """
-            {
-                "resource_name": "aplprdebawas01",
+                "event_time": "2021-06-27 02:20:50",
+                "host_ip": "apne1-insfrprd-ecp-ecprds.cfqdcjczxddr.ap-northeast-1.rds.amazonaws.com",
+                "summary": "07. DB그룹>10.INNISFREE GLOBAL>PostgreSQL 10 (apne1-insfrprd-ecp-ecprds.cfqdcjczxddr.ap-northeast-1.rds.amazonaws.com : INSFRPRDDB)>Databases>INSFRPRDDB",
+                "event_id": "32326983",
+                "status": "UP ",
+                "urgency": "1",
+                "metric_value": "Rollback 수 [주의 > 0 Count/s, 해제 = 0 Count/s] ",
+                "severity": "주의",
+                "threshold": "INSFRPRDDB",
+                "conditionlog": "Rollback 수 [0.02 Count/s (> 0 Count/s)]",
+                "metric_name": "롤백 발생 "
+
+
+                "metric_value": "이벤트 탐지 [심각도: ERROR , 개행 포함, 내용 패턴: (?=.*member.*)(^((?!MLEC_MEMBER_NOT_FOUND).)*$)] ",
+                "summary": "01. APMALL>EAPWAS-172.28.103.146>monitor group>ecp-api.log_member",
+                "threshold": "ecp-api.log_member",
+                "event_time": "2021-06-27 02:17:10",
+                "status": "UP ",
+                "conditionlog": "[1회 발생] 이벤트 탐지 [심각도: ERROR, 내용: [2021-06-27 02:17:08,176] [ERROR] [ http-nio-8080-exec-5] [B2CMON_LOG.logging:124] [MON|2021-06-27 02:17:08,176|ERROR|M01||MobileWeb|member||||MLEC_MEMBER_AUTH_FAILURE|/MON] Fail. API=post:/v1/M01/ap/members/joinOnLogin, resultMsg=Authentication Failure [MemberUtils.memberMonLoggingError:457 < ApMemberAuthRestApi.login:318 < ApMemberAuthRestApi.joinOnLogin:415 < GeneratedMethodAccessor4391.invoke:-1 < DelegatingMethodAccessorImpl.invoke:43]]",
+                "host_ip": "172.28.103.146",
+                "metric_name": "회원_경고로그 ",
+                "severity": "경고",
+                "resource_name": "EAPWAS-172.28.103.146",
                 "urgency": "2",
-                "status": "UP",
-                "host_ip": "172.28.102.12",
-                "threshold": "ecp-batch.log_delivery",
-                "summary": "[경고]02. ECP(APMALL&ETUDE)>APNE2-DSPPRD-A-ECP-EBAWAS-01>monitor group>ecp-batch.log_delivery - [1회 발생] 이벤트 탐지 [심각도: ERROR, 내용: [2020-09-25 07:46:32,155] [ERROR] [schedulerForBatch_Worker-21] [B2CMON_LOG.logging:124] [MON||ERROR|M02|||delivery|||2020-09-25 07:46:32,155|MLEC_EORD056|/MON] 21042564 [OrderServiceImpl.deliveryInfoMonLogging:20779 < OrderServiceImpl$$FastClassBySpringCGLIB$$3c44f530.invoke:-1 < MethodProxy.invoke:204 < CglibAopProxy$CglibMethodInvocation.invokeJoinpoint:738 < ReflectiveMethodInvocation.proceed:157]]",
-                "event_time": "2020-09-25 07:46:32",
-                "metric_value": "이벤트 탐지 [심각도: ERROR , 개행 포함, 내용 패턴: .*delivery.*MLEC_.*] ",
-                "event_id": "21296053",
-                "metric_name": "배송_경고로그 "
-            }
+                "event_id": "32326947"
 
         """
         default_parsed_data_list = []
@@ -37,7 +51,6 @@ class EventManager(BaseManager):
         ip_address = raw_data.get('host_ip')
         resource_id = raw_data.get('resource_name')
         metric_value = raw_data.get('metric_value')
-        summary = raw_data.get('summary')
         event_resource_vo = {}
 
         try:
@@ -49,7 +62,7 @@ class EventManager(BaseManager):
                 event_resource_vo.update({'resource_id': resource_id})
                 event_resource_vo.update({'name': f'{resource_id}'})
 
-            parsed_summary = self._parse_summary(summary)
+            parsed_summary = self._parse_summary(raw_data)
 
             event_vo = {
                 'event_key': event_key,
@@ -115,22 +128,9 @@ class EventManager(BaseManager):
         return parsed_occurred_at
 
     @staticmethod
-    def _parse_summary(raw_data_summary):
-        parsed_summary = {}
-        if raw_data_summary is not None:
-            first_parsing_index = re.search(r"\d", raw_data_summary)
-            if first_parsing_index is not None:
-                title_first_idx = first_parsing_index.start()
-                title_last_idx = None
-                for meta in TITLE_PARSING_META:
-                    parse_temp_meta = raw_data_summary.find(meta)
-                    if parse_temp_meta > -1:
-                        title_last_idx = parse_temp_meta
-                        break
-
-            title = raw_data_summary[title_first_idx:title_last_idx]
-            parsed_summary.update({
-                'title': title,
-                'body': raw_data_summary[title_last_idx + 1:]
-            })
+    def _parse_summary(raw_data):
+        parsed_summary = {
+            'title': raw_data.get('summary', ''),
+            'body': raw_data.get('conditionlog', '')
+        }
         return parsed_summary
