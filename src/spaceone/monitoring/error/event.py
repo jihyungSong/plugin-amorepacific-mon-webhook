3a97ae15e425ad6790201d2aa19da1b101beca01
jean1042 <jean1042@naver.com>
2021-10-21 18:36:41 +0900

    refactor: event model changes

--- src/spaceone/monitoring/error/event.py
+++ src/spaceone/monitoring/error/event.py
@@ -1,1 +1,10 @@
 from spaceone.core.error import *
+
+
+class ERROR_CHECK_VALIDITY(ERROR_BASE):
+    _message = 'Event model is not validate (field= {field})'
+
+
+class ERROR_REQUIRED_FIELDS(ERROR_BASE):
+    _message = 'Required field is missing(field= {field})'
+
--- src/spaceone/monitoring/info/event_info.py
+++ src/spaceone/monitoring/info/event_info.py
@@ -6,21 +6,21 @@ from spaceone.core.pygrpc.message_type import *
 __all__ = ['EventInfo', 'EventsInfo']
 
 
-def EventInfo(event_Info_data: EventModel):
-
+def EventInfo(event_info_data: EventModel):
     info = {
-        'event_key': event_Info_data['event_key'],
-        'event_type': event_Info_data['event_type'],
-        'description': event_Info_data.get('description'),
-        'title': event_Info_data['title'],
-        'severity': event_Info_data['severity'],
-        'resource': change_struct_type(event_Info_data['resource']),
-        'rule': event_Info_data.get('rule'),
-        'occurred_at': utils.datetime_to_iso8601(event_Info_data.get('occurred_at')),
-        'additional_info': change_struct_type(event_Info_data.get('additional_info'))
+        'event_key': event_info_data['event_key'],
+        'event_type': event_info_data['event_type'],
+        'description': event_info_data.get('description'),
+        'title': event_info_data['title'],
+        'severity': event_info_data['severity'],
+        'resource': change_struct_type(event_info_data['resource']),
+        'rule': event_info_data.get('rule'),
+        'occurred_at': utils.datetime_to_iso8601(event_info_data.get('occurred_at')),
+        'additional_info': change_struct_type(event_info_data.get('additional_info'))
     }
+
     return event_pb2.EventInfo(**info)
 
 
-def EventsInfo(event_Info_datas, **kwargs):
-    return event_pb2.EventsInfo(results=list(map(functools.partial(EventInfo, **kwargs), event_Info_datas)))
+def EventsInfo(event_info_datas, **kwargs):
+    return event_pb2.EventsInfo(results=list(map(functools.partial(EventInfo, **kwargs), event_info_datas)))
